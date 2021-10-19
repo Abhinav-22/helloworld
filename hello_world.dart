868629ca61b74e80118f65d20164c0c87c2f49c3
Aman-Clement <amanclementaranha@gmail.com>
2021-10-19 18:44:19 +0530

    hello world in dart

---
+++ hello_world.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+runApp(MyHomePage());
+}
+
+class MyHomePage extends StatelessWidget{
+Widget build(BuildContext context){
+	return MaterialApp(
+	home: Center(
+		child: Text('Hello World')
+	),
+	);
+}
+}
